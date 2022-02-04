7a5b4c6d1c6f916e5411ab3734e2e7fe147a7248
MohamedAbdi-Hirsi2 <97524537+MohamedAbdi-Hirsi2@users.noreply.github.com>
2022-02-04 15:10:43 +0300

    Add files via upload

---
+++ pen_drawing_2022_02_01_11_20_10/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+}
+
+function draw() {
+  
+  noStroke();
+  fill(2,700,40);
+  circle(mouseX,mouseY,24);
+}
+
+function mousePressed(){
+  background(0);
+  
+}
